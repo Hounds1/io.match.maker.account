f4aa6333e70e837a3ae9ea88100511681b3f5dca
Hounds1 <zoodisx@gmail.com>
2024-05-29 21:56:09 +0900

    git init

---
+++ src/test/java/io/exam/match/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package io.exam.match.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
